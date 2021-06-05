68aaa961566ebdf20788fde5052660f9f97e7fb0
kaustubh0201 <kaustubh1818@gmail.com>
2021-06-05 22:39:20 +0530

    kalman filter implemented

--- mpu2.c
+++ mpu2.c
@@ -2,9 +2,7 @@
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 
-
 Adafruit_MPU6050 mpu;
-float RADIANS_TO_DEGREES = 180/3.14159;
 
 float base_x_accel;
 float base_y_accel;
@@ -14,6 +12,9 @@ float base_x_gyro;
 float base_y_gyro;
 float base_z_gyro;
 
+
+
+float RADIANS_TO_DEGREES = 180/3.14159;
 
 typedef struct {
     float ax;
@@ -30,11 +31,9 @@ float last_x_angle;
 float last_y_angle;
 float last_z_angle;    
 
-float last_gyro_x_angle;
+float last_gyro_x_angle;    
 float last_gyro_y_angle;
 float last_gyro_z_angle;
-
-/********************** KALMAN FILTER ************************/
 
 class Filter {
     private:
@@ -115,11 +114,6 @@ class Filter {
 Filter xFilter;
 Filter yFilter;
 
-
-/********************** END KALMAN FILTER ************************/
-
-
-
 void set_last_read_angle_data(unsigned long time, float x, float y, float z, 
                              float x_gyro, float y_gyro, float z_gyro) {
     last_read_time = time;
@@ -131,21 +125,8 @@ void set_last_read_angle_data(unsigned long time, float x, float y, float z,
     last_gyro_z_angle = z_gyro;
 }
 
-void read(sensorValue * sv){
-    sensors_event_t a, g, temp;
-    mpu.getEvent(&a, &g, &temp);
-
-    sv->ax = a.acceleration.x; 
-    sv->ay = a.acceleration.y; 
-    sv->az = a.acceleration.z; 
-    
-    sv->gx = g.gyro.x; 
-    sv->gy = g.gyro.y; 
-    sv->gz = g.gyro.z; 
-}
-
 void calibrate_sensors() {
-    int num_readings = 100;
+    int   num_readings = 10;
     
     float x_accel = 0;
     float y_accel = 0;
@@ -169,7 +150,7 @@ void calibrate_sensors() {
         x_gyro += accel_t_gyro.gx;
         y_gyro += accel_t_gyro.gy;
         z_gyro += accel_t_gyro.gz;
-        delay(150);
+        delay(100);
     }
 
     x_accel /= num_readings;
@@ -186,25 +167,99 @@ void calibrate_sensors() {
     base_x_gyro = x_gyro;
     base_y_gyro = y_gyro;
     base_z_gyro = z_gyro;
+}
+
+void read(sensorValue * sv){
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+
+    sv->ax = a.acceleration.x; 
+    sv->ay = a.acceleration.y; 
+    sv->az = a.acceleration.z; 
     
-    
-    
-    
+    sv->gx = g.gyro.x; 
+    sv->gy = g.gyro.y; 
+    sv->gz = g.gyro.z; 
 }
 
 void setup(void) {
     Serial.begin(115200);
     while (!Serial)
-        delay(10); 
+        delay(10); // will pause Zero, Leonardo, etc until serial console opens
 
+    // Serial.println("Adafruit MPU6050 test!");
+
+    // Try to initialize!
     if (!mpu.begin()) {
+        //Serial.println("Failed to find MPU6050 chip");
         while (1) {
             delay(10);
         }
     }
+    //Serial.println("MPU6050 Found!");
+
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    //Serial.print("Accelerometer range set to: ");
+    switch (mpu.getAccelerometerRange()) {
+    case MPU6050_RANGE_2_G:
+        //Serial.println("+-2G");
+        break;
+    case MPU6050_RANGE_4_G:
+        //Serial.println("+-4G");
+        break;
+    case MPU6050_RANGE_8_G:
+        //Serial.println("+-8G");
+        break;
+    case MPU6050_RANGE_16_G:
+        //Serial.println("+-16G");
+        break;
+    }
+    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+    //Serial.print("Gyro range set to: ");
+    switch (mpu.getGyroRange()) {
+    case MPU6050_RANGE_250_DEG:
+        //Serial.println("+- 250 deg/s");
+        break;
+    case MPU6050_RANGE_500_DEG:
+     //Serial.println("+- 500 deg/s");
+        break;
+    case MPU6050_RANGE_1000_DEG:
+        //Serial.println("+- 1000 deg/s");
+        break;
+    case MPU6050_RANGE_2000_DEG:
+        //Serial.println("+- 2000 deg/s");
+        break;
+    }
+
+    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+    //Serial.print("Filter bandwidth set to: ");
+    switch (mpu.getFilterBandwidth()) {
+    case MPU6050_BAND_260_HZ:
+        //Serial.println("260 Hz");
+        break;
+    case MPU6050_BAND_184_HZ:
+        //Serial.println("184 Hz");
+        break;
+    case MPU6050_BAND_94_HZ:
+        //Serial.println("94 Hz");
+        break;
+    case MPU6050_BAND_44_HZ:
+        //Serial.println("44 Hz");
+        break;
+    case MPU6050_BAND_21_HZ:
+        //Serial.println("21 Hz");
+        break;
+    case MPU6050_BAND_10_HZ:
+        //Serial.println("10 Hz");
+        break;
+    case MPU6050_BAND_5_HZ:
+     // Serial.println("5 Hz");
+        break;
+    }
 
     calibrate_sensors();
-    set_last_read_angle_data(millis(), 0, 0, 0, 0, 0, 0);    
+    set_last_read_angle_data(millis(), 0, 0, 0, 0, 0, 0);
+    
 
     delay(100);
 }
@@ -222,32 +277,39 @@ void loop() {
     read(&sv);
     unsigned long t_now = millis();
 
-    //Get gyro angle and convert to degree
-    float gyro_x = (sv.gx - base_x_gyro) * RADIANS_TO_DEGREES;
-    float gyro_y = (sv.gy - base_y_gyro) * RADIANS_TO_DEGREES;
-    float gyro_z = (sv.gz - base_z_gyro) * RADIANS_TO_DEGREES;
+    float gyro_x = (sv.gx - base_x_gyro)* RADIANS_TO_DEGREES;
+    float gyro_y = (sv.gy - base_y_gyro)* RADIANS_TO_DEGREES;
+    float gyro_z = (sv.gz - base_z_gyro)* RADIANS_TO_DEGREES;
     
-    //Get acc value
+    
+    //Get raw acceleration values
+    //float G_CONVERT = 16384;
     float accel_x = sv.ax;
     float accel_y = sv.ay;
     float accel_z = sv.az;
 
-    //Calculate x and y angle from accelerometer reading 
     float accel_angle_y = getYangle(accel_x, accel_y, accel_z);
     float accel_angle_x = getXangle(accel_x, accel_y, accel_z);
+    
     float accel_angle_z = 0;
 
     float dt = (t_now - last_read_time)/1000.0;
 
-    //Complementary Filter
+    //Filtered
     float gyro_angle_x = gyro_x*dt + last_x_angle;
     float gyro_angle_y = gyro_y*dt + last_y_angle;
     float gyro_angle_z = gyro_z*dt + last_z_angle;
     
+    //Unfiltered
+    float unfiltered_gyro_angle_x = gyro_x*dt + last_gyro_x_angle;
+    float unfiltered_gyro_angle_y = gyro_y*dt + last_gyro_y_angle;
+    float unfiltered_gyro_angle_z = gyro_z*dt + last_gyro_z_angle;
+
     float alpha = 0.96;
-    float angle_x = alpha * gyro_angle_x + (1.0 - alpha) * accel_angle_x;
-    float angle_y = alpha * gyro_angle_y + (1.0 - alpha) * accel_angle_y;
-    float angle_z = gyro_angle_z; 
+    float angle_x = alpha*gyro_angle_x + (1.0 - alpha)*accel_angle_x;
+    float angle_y = alpha*gyro_angle_y + (1.0 - alpha)*accel_angle_y;
+    float angle_z = gyro_angle_z;    //Accelerometer doesn't give z-angle
+
 
     //Kalman Filter
     
@@ -260,36 +322,30 @@ void loop() {
     yFilter.beginFiltering(dt, accel_angle_y, gyro_y);
 
 
-    //Unfiltered
-    float unfiltered_gyro_angle_x = gyro_x * dt + last_gyro_x_angle;
-    float unfiltered_gyro_angle_y = gyro_y * dt + last_gyro_y_angle;
-    float unfiltered_gyro_angle_z = gyro_z * dt + last_gyro_z_angle;
-
-  
-
     set_last_read_angle_data(t_now, angle_x, angle_y, angle_z, unfiltered_gyro_angle_x, unfiltered_gyro_angle_y, unfiltered_gyro_angle_z);
     // Send the data to the serial port
-
-    // Serial.write((byte *) &angle_x, 4);
-    // Serial.write((byte *) &angle_y, 4);
+    Serial.print(F("DEL:"));                            //Delta T
+    Serial.print(dt, DEC);
+    Serial.print(F("#ACC:"));                            //Accelerometer angle
+    Serial.print(accel_angle_x, 2);
+    Serial.print(F(","));
+    Serial.print(accel_angle_y, 2);
+    Serial.print(F(","));
+    Serial.print(accel_angle_z, 2);
+    Serial.print(F("#GYR:"));
+    Serial.print(unfiltered_gyro_angle_x, 2);                //Gyroscope angle
+    Serial.print(F(","));
+    Serial.print(unfiltered_gyro_angle_y, 2);
+    Serial.print(F(","));
+    Serial.print(unfiltered_gyro_angle_z, 2);
+    Serial.print(F("#FIL:"));                         //Filtered angle
+    Serial.print(xFilter.angle, 2);
+    Serial.print(F(","));
+    Serial.print(yFilter.angle, 2);
+    Serial.print(F(","));
+    Serial.print(angle_z, 2);
+    Serial.println(F(""));
     
-    Serial.print(" |GYRO| ");
-    Serial.print(gyro_angle_x);
-    Serial.print(", ");
-    Serial.print(gyro_angle_y);
-    Serial.print(" |ACC| ");
-    Serial.print(accel_angle_x);
-    Serial.print(", ");
-    Serial.print(accel_angle_y);
-    Serial.print(" |COMP FILTER| ");
-    Serial.print(angle_x);
-    Serial.print(", ");
-    Serial.print(angle_y);
-    Serial.print(" |KALMAN FILTER| ");
-    Serial.print(xFilter.angle);
-    Serial.print(", ");
-    Serial.print(yFilter.angle);
-    Serial.print("\n");
-
-    delay(150);
+    // Delay so we don't swamp the serial port
+    delay(5);
 }
